b51a322966b6b3d820821c0aeca68ccec59b0964
fabricio covalesci <fabcovalesci@gmail.com>
2022-03-19 15:57:12 -0300

    adicionando paginas html

--- modelo/urls.py
+++ modelo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from modelo import views as v
 
+app_name = 'modelo'
 
 urlpatterns = [
-
+    path('', v.index, name='modelo_index'),
 ]
--- modelo/views.py
+++ modelo/views.py
@@ -1,3 +1,11 @@
+from logging import NullHandler
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView, UpdateView
 
+class ModeloIndex(ListView):
+    model = NullHandler
+    template_name = 'index.html'
+
+def index(request):
+    return render(request, 'modelo/index.html')
 
